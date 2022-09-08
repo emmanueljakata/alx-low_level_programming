23f17032fe2d555abb98affcea176a5f2f148a50
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-08 16:45:19 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A Quote
+ * 
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
