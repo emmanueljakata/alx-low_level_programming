308e65d3c3b27db961b21dc87300c52dc06a45ae
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-08 15:43:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(void)
-/* Task four */
+/**
+ * main - Entry point
+ *
+ * Description: 'Program to print using puts instead of printf'
+ *
+ * Return: 0 (Success)
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
