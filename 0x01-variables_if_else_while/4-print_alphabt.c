65ad01134943077bfd82ac8223ecbf06d07974ec
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 16:40:05 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - alphabrt ecluding e and q
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph != 'e' 'q' alph <= 'z'; alph++)
+		putchar(alp);
+	putchar('\n')
+	return (0);
+}
