779364e9207f41078d73325ec0883ff4119c40f0
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 12:39:39 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,28 +18,17 @@ int main(void)
 
 {
 
-		int n;
+	int n;
 
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					if (n>0)
-					{
-					printf("is positive\n");
-					}
-					
-					else if(n==0)
-					{
-					printf("is zero\n");
-					}
-
-					else(n<0)
-					{
-					printf("is negetive\n");
-					}
-
-					return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n>0)
+		printf("is positive");	
+	else if (n==0)
+		printf("is zero");
+	else (n<0)
+		printf("is negetive");
+	printf("\n");
+	return (0);
 }
