9c5e6c131c68c52fd57f52ae6f79a58ffc137df4
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 16:54:17 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,12 @@ int main(void)
 {
 	char alph;
 
-	for (alph = 'a'; alph != 'e'; alph != 'q'; alph <= 'z'; alph++)
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'e' || alph == 'q')
+			continue;
 		putchar(alph);
+	}
 	putchar('\n');
 	return (0);
 }
