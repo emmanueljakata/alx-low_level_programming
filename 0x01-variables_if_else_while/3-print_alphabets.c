2f801c49f76e396abe10d351b4e9dfa53093b452
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 16:25:20 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Alphabet in lowercase then uppercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alph++)
+		putchar(alph);
+	putchar('\n')
+	return (0);
+}
