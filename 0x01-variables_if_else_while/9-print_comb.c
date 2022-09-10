4f6db58fc74f42546902c853192ea70d133d9f25
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 18:39:39 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Number combination
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	{
+		putchar((char)digit);
+	if (digit != 57)
+		{												putchar(',');
+														putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
