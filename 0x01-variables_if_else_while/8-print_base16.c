6fa1f4c5d4eeb86b26a0788bdee6de567ead12d9
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 18:15:53 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - base sixteen hexadecimal
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char hex;
+
+	for (hex = 0; hex <= 9, hex++)
+		putchar(hex);
+	for (hex = 'a'; hex <= 'f'; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
