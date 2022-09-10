e8f7da263d40fb98622396ae3640443d5e8ea872
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 17:11:53 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poin
+ * Discription - print numbers in baseten from zero
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
