f05791d9d18fbb67c8d1b1bafd7fcd83ff666775
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-09 17:30:19 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Dicription- Basten digits using putchar
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	int digit;
+	for(digit = 48; digit <= 57; digit++)
+		putchar((char)digit);
+	putchar('\n');
+	return (0);
+}
