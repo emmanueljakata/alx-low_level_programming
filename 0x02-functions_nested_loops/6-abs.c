d4efed620ef53d9991f81e493244077fea465d5c
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-14 13:16:10 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ * @i: number targeted
+ * Return: returns 0
+ */
+
+int _abs(int i)
+
+{
+	if (i > 0)
+		return (i);
+
+	else if (i < 0)
+		return (-i);
+
+	else
+		return (0);
+}
