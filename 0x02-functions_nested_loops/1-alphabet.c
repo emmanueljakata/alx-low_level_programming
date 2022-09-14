c0cb7ee1eaa99a313f8fa871db694dd248a3443c
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-13 18:53:25 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Discription - print alphabet
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int u;
+
+	for (u = 'a'; u <= 'z'; u++)
+	{
+		_putchar(u);
+	}
+	_putchar('\n');
+
+return (0);
+}
