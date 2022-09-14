dfc38401bd35942df27b83b205c00c9a39431a5c
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-13 18:26:39 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,18 @@
  * Discription - program that prints _putchar
  * Return: 0 (success)
  */
-int _putchar(char c)
+int main(void)
 
 {
-	return(write(1, &c, 1));
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar("\n")
+
+	return (0);
 }
