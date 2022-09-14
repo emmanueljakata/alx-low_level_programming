7a77b15439f183c6a5fe79877831fda8dd4f80e3
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-14 12:09:29 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to print
+ * Return: On success 1
+ */
+
+int _isalpha(int c)
+
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+return (1);
+
+return (0);
+}
