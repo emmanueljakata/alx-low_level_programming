bdebdf311cab45f60cf146dc47a1e3ea23fd8aa2
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-14 12:36:45 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * print_sign - function that prints the sign of a number
+ * @c: character to be verified
+ * Return: return 0 or 1
  */
 
 int print_sign(int n)
 {
-	if (n >0)
+	if (n > 0)
 	{
 		_putchar('+');
-		return (1)
+		return (1);
 	{
 	else if (n == 0)
-		_putchar('0')
-		return 0
+		_putchar('0');
+		return (0);
 	
 	}
 	else
 	{
-		putchar('_');
-		return (-1)
-
-
-
+		putchar('-');
+		return (-1);
  }
