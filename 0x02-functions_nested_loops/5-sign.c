8a5e00970b22ae70455c48b648e92a5856699a16
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-14 12:45:10 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,9 +12,9 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	{
+	}
 	else if (n == 0)
-		_putchar('0');
+	{	_putchar('0');
 		return (0);
 	
 	}
@@ -22,4 +22,5 @@ int print_sign(int n)
 	{
 		putchar('-');
 		return (-1);
+	}
  }
