3de8c38de19636988e0aec031fdd7cfdf08860c1
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-13 19:21:11 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  -
+ */
+
+void print_alphabet_x10(void)
+{
+	int u;
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+		for b(u = 'a'; u <= 'z'; u++)
+		{
+			putchar(u);
+		}
+		_putchar('\n);
+	}
+}
