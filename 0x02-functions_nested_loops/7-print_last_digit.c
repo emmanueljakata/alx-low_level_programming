076e564cd6706384e4e40b33ba78baf356d32f9a
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-14 14:04:09 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,14 @@ int print_last_digit(int n)
 
 {
 	int l;
-
 	if (n < 0)
 		n = -n;
-	l = n % 10;
-
-	_putchar (l + '0');
-	return (1);
+	
+	a = n % 10;
+	
+	if (l < 0)
+		l = -l;
+	_putchar(l + '0');
+	
+	return (l);
 }
