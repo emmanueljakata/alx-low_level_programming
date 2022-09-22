3dfb715a6449afcb0965028d4125350850a2e5b9
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-09-22 15:10:13 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,8 @@
 
 /**
  * _strcat - appends src string to dest string
- *
  * @dest: string to append by src
- *
  * @src: string to append to dest
- *
  * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
@@ -15,12 +12,15 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = j = 0;
+
 	while (*(dest + i))
 		i++;
+
 	while ((*(dest + i) = *(src + j)))
 	{
 		i++;
 		j++;	
 	}
+	
 	return (dest);
 }
