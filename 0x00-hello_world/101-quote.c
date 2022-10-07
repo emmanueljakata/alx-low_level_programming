2d635b14a13c5f1b394e4ddd3da2b38be5d7c681
Emmanuel Jakata <emmanueljakata@gmail.com>
2022-10-07 07:18:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-	printf(stderr, "%s", "and that piece of art is useful\n"");
-	printf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+	printf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
